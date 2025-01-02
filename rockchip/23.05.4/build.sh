b37baf087087ffdff4506182b96dd9151a8c0082
wvsley <wvsley@gmail.com>
2025-01-02 21:53:25 +0800

    build.sh

--- rockchip/23.05.4/build.sh
+++ rockchip/23.05.4/build.sh
@@ -34,7 +34,6 @@ PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-minidlna-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-smartdns-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-rclone-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-modemband-zh-cn"
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
